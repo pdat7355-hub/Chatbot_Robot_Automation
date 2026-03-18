06debc9a39a25b3d97b36a0d9efc1f962f2fd137
pdat7355-hub <pdat7355@gmail.com>
2026-03-18 14:13:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 require('dotenv').config();
-const app = require('./src/app');
+const app = require('./src/app'); // Gọi đến src/app.js
 
-// Render sẽ tự cấp PORT thông qua biến môi trường. Nếu không có thì dùng 3000 (local).
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`-----------------------------------------`);
-    console.log(`🚀 Server Shop Hương Kid đã sẵn sàng!`);
-    console.log(`📍 Cổng: ${PORT}`);
-    console.log(`-----------------------------------------`);
+    console.log(`🚀 Server đang chạy tại cổng: ${PORT}`);
 });
