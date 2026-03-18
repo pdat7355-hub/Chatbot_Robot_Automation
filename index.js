d2e7d72e1b29220c8b6901bfa6e1bc01f4195ee9
pdat7355-hub <pdat7355@gmail.com>
2026-03-18 14:21:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
-const app = require('./src/app'); // Gọi đến src/app.js
+const app = require('./src/app'); // Đúng đường dẫn đến src/app.js
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`🚀 Server đang chạy tại cổng: ${PORT}`);
+    console.log(`-----------------------------------------`);
+    console.log(`🚀 Server Shop Hương Kid đang chạy!`);
+    console.log(`📍 Port: ${PORT}`);
+    console.log(`-----------------------------------------`);
 });
